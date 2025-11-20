badfbc0707db2ccfd8ee4e2d11602747213c83d4
thPhonG-oss <ngthanhphong0817@gmail.com>
2025-11-20 22:16:52 +0700

    change in employeeRepository

--- BE/src/main/java/com/hr_management/hr_management/repository/EmployeeRepository.java
+++ BE/src/main/java/com/hr_management/hr_management/repository/EmployeeRepository.java
@@ -3,5 +3,5 @@ package com.hr_management.hr_management.repository;
 import com.hr_management.hr_management.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
