0bf7dd990c91eb0ecfc65be9579e28f49452ba24
Cong Chau <congchau206#gmail.com>
2025-11-22 01:40:45 +0700

    UI list account [ADMIN]

--- FE/src/router/roles/adminRoutes.jsx
+++ FE/src/router/roles/adminRoutes.jsx
@@ -1,6 +1,7 @@
 import AdminLayout from "../../components/layout/AdminLayout";
 import DashboardAdmin from "../../pages/admin/Dashboard";
 import EmployeeManagement from "../../pages/admin/EmployeeManagement";
+import AccountManagement from "../../pages/admin/AccountManagement";
 
 import RoleRoute from "../RoleRoute";
 
@@ -15,6 +16,7 @@ const adminRoutes = [
     children: [
       { path: "dashboard", element: <DashboardAdmin /> },
       { path: "employee-management", element: <EmployeeManagement /> },
+      { path: "employee-accounts", element: <AccountManagement /> },
 
       // thêm các route admin khác ở đây
     ],
