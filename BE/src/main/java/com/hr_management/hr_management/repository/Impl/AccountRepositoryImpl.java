20a8a8052873d26b3c82d4c34af5a748001f11a8
thPhonG-oss <ngthanhphong0817@gmail.com>
2025-11-21 22:44:20 +0700

    resolve conflict in AccountRepositoryImpl.java

--- BE/src/main/java/com/hr_management/hr_management/repository/Impl/AccountRepositoryImpl.java
+++ BE/src/main/java/com/hr_management/hr_management/repository/Impl/AccountRepositoryImpl.java
@@ -1,13 +1,12 @@
 package com.hr_management.hr_management.repository.Impl;
 
-<<<<<<< HEAD
+
 import com.hr_management.hr_management.dto.response.AccountResponseDTO;
 import com.hr_management.hr_management.repository.AccountRepository;
 import com.hr_management.hr_management.service.AccountService;
-=======
+
 import com.hr_management.hr_management.repository.AccountRepository;
 
->>>>>>> main
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
