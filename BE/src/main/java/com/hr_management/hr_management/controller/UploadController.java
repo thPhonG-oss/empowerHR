255ef7a81b83cc56cecf229845c6562b31c82cc0
thPhonG-oss <ngthanhphong0817@gmail.com>
2025-11-24 04:13:28 +0700

    fix(Cloudinary): add @PathVaribale for folderName

--- BE/src/main/java/com/hr_management/hr_management/controller/UploadController.java
+++ BE/src/main/java/com/hr_management/hr_management/controller/UploadController.java
@@ -5,10 +5,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -17,12 +14,12 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-@RequestMapping("/api/upload")
+@RequestMapping("/api/v1/upload")
 public class UploadController {
     CloudinaryService cloudinaryService;
 
-    @PostMapping
-    public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file, String folderName)throws IOException {
+    @PostMapping("/{folderName}")
+    public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file,@PathVariable String folderName)throws IOException {
         String url = cloudinaryService.uploadFile(file, folderName);
         return ResponseEntity.ok(Map.of("url", url));
     }
