22dd0a5ea5bcda0059aac78d604093d7985cfaab
thPhonG-oss <ngthanhphong0817@gmail.com>
2025-11-27 21:48:01 +0700

    set-up: complete set up sprint 2

---
+++ BE/src/main/java/com/hr_management/hr_management/dto/response/RequestResponseDTO.java
@@ -0,0 +1,23 @@
+package com.hr_management.hr_management.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder // Dùng SuperBuilder để các con kế thừa được
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestResponseDTO {
+    private Integer requestId;
+    private String status;
+    private String requestType; // "Leave" hoặc "TimesheetUpdate"
+    private String reason;
+    private String employeeName;
+    private String employeeCode;
+    private String handleAt;
+    private String submitAt;
+    private String responseReason;
+}
