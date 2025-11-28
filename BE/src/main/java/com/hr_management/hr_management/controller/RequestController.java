b00689264607265c88a87c4eff5974bc8be2cca8
thPhonG-oss <ngthanhphong0817@gmail.com>
2025-11-28 20:19:18 +0700

    feat: complete api view details request

--- BE/src/main/java/com/hr_management/hr_management/controller/RequestController.java
+++ BE/src/main/java/com/hr_management/hr_management/controller/RequestController.java
@@ -1,9 +1,14 @@
 package com.hr_management.hr_management.controller;
 
+import com.hr_management.hr_management.dto.request.ApiResponse;
 import com.hr_management.hr_management.service.RequestService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class RequestController {
     RequestService requestService;
+
+    @GetMapping("/{requestId}")
+    public ResponseEntity<Object> getRequestById(@PathVariable("requestId") Integer requestId){
+        return ResponseEntity.ok()
+                .body(
+                        ApiResponse.builder()
+                                .code("1000")
+                                .message("Success")
+                                .result(requestService.getRequestById(requestId))
+                                .build()
+                );
+    }
 }
--- BE/src/main/java/com/hr_management/hr_management/service/Impl/RequestServiceImpl.java
+++ BE/src/main/java/com/hr_management/hr_management/service/Impl/RequestServiceImpl.java
@@ -1,5 +1,10 @@
 package com.hr_management.hr_management.service.Impl;
 
+import com.hr_management.hr_management.dto.response.RequestResponseDTO;
+import com.hr_management.hr_management.entity.Request;
+import com.hr_management.hr_management.exception.AppException;
+import com.hr_management.hr_management.exception.ErrorCode;
+import com.hr_management.hr_management.mapper.RequestMapper;
 import com.hr_management.hr_management.repository.RequestRepository;
 import com.hr_management.hr_management.service.RequestService;
 import lombok.AccessLevel;
@@ -12,4 +17,13 @@ import org.springframework.stereotype.Service;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class RequestServiceImpl implements RequestService {
     RequestRepository requestRepository;
+    RequestMapper requestMapper;
+
+    @Override
+    public RequestResponseDTO getRequestById(Integer requestId){
+        Request request = requestRepository.findById(requestId)
+                .orElseThrow(() -> new AppException(ErrorCode.REQUEST_NOT_FOUND));
+
+        return requestMapper.toResponseDTO(request);
+    }
 }
