437c497a5bbe07949de575b323494cab50cd1ce3
thPhonG-oss <ngthanhphong0817@gmail.com>
2025-11-28 20:03:23 +0700

    add RequestMapping into request controller

--- BE/src/main/java/com/hr_management/hr_management/controller/RequestController.java
+++ BE/src/main/java/com/hr_management/hr_management/controller/RequestController.java
@@ -4,9 +4,11 @@ import com.hr_management.hr_management.service.RequestService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/requests")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class RequestController {
