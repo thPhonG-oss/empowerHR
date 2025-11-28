36280aacbc7af81948424d7585fbf55f9beb8a34
thPhonG-oss <ngthanhphong0817@gmail.com>
2025-11-28 19:35:21 +0700

    update(Request): injection in to RequestController, RequestServiceImpl

--- BE/src/main/java/com/hr_management/hr_management/controller/RequestController.java
+++ BE/src/main/java/com/hr_management/hr_management/controller/RequestController.java
@@ -1,5 +1,6 @@
 package com.hr_management.hr_management.controller;
 
+import com.hr_management.hr_management.service.RequestService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -9,4 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class RequestController {
+    RequestService requestService;
 }
