83d2ca0e25fc39aea395258979a5e37552a2513a
thPhonG-oss <ngthanhphong0817@gmail.com>
2025-11-30 23:49:55 +0700

    fix: resolved conflict

--- BE/src/main/java/com/hr_management/hr_management/controller/RequestController.java
+++ BE/src/main/java/com/hr_management/hr_management/controller/RequestController.java
@@ -7,12 +7,9 @@ import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,12 +23,23 @@ public class RequestController {
     RequestService requestService;
 
     @GetMapping("/{requestId}")
-    public ResponseEntity<Object> getRequestById(@PathVariable("requestId") Integer requestId){}
+    public ResponseEntity<Object> getRequestById(@PathVariable("requestId") Integer requestId){
+        return null;
+    }
     @GetMapping("/handled")
     public ResponseEntity<Object> getHandledRequests(
             @RequestParam(name = "pageNumber", required = false, defaultValue = "1") Integer pageNumber,
             @RequestParam(name = "pageSize", required = false, defaultValue = "10") Integer pageSize
-    ) { }
+    ) {
+        return ResponseEntity.ok()
+                .body(
+                        ApiResponse.builder()
+                                .code("1000")
+                                .message("Success")
+                                .result(requestService.getAllHandledRequests(pageNumber, pageSize))
+                                .build()
+                );
+    }
     @PatchMapping("/{requestId}")
     public ResponseEntity<Object> handleRequest(@RequestBody @Valid RequestHandleDTO requestHandleDTO, @PathVariable(name = "requestId") Integer requestId){
         return ResponseEntity.ok()
@@ -40,7 +48,6 @@ public class RequestController {
                                 .code("1000")
                                 .message("Success")
                                 .result(requestService.getRequestById(requestId))
-                                .result(requestService.getAllHandledRequests(pageNumber, pageSize))
                                 .build()
                 );
     }
