3db6f15f1d5a8249046999d9a7d9baa021112031
thPhonG-oss <ngthanhphong0817@gmail.com>
2025-12-12 23:18:42 +0700

    resolved duplicate enum in ErrorCode.java

--- BE/src/main/java/com/hr_management/hr_management/exception/ErrorCode.java
+++ BE/src/main/java/com/hr_management/hr_management/exception/ErrorCode.java
@@ -44,7 +44,6 @@ public enum ErrorCode {
     NOT_CHECKIN("1019","haven't checked in yet",HttpStatus.BAD_REQUEST),
     CHECKOUT_ERROR("1020","checked out today ",HttpStatus.BAD_REQUEST),
     LEAVE_BALANCE_NOT_FOUND("1026","LEAVE_BALANCE_NOT_FOUND",HttpStatus.BAD_REQUEST),
-    LEAVE_BALANCE_NOT_FOUND("1021","LEAVE_BALANCE_NOT_FOUND",HttpStatus.BAD_REQUEST),
     STRAVA_TOKEN_EXCHANGE_FAILED("1021","Strava token exchange failed",HttpStatus.BAD_REQUEST),
     STRAVA_ALREADY_CONNECTED("1022","Strava already connected",HttpStatus.BAD_REQUEST),
     INVALID_STRAVA_STATE("1023","Invalid Strava state parameter",HttpStatus.BAD_REQUEST),
