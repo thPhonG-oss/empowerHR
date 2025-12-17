c56076c469a64ac5967f417a27fcfe5131922bec
Do Ngoc Cuong <cuong01656499081@gmail.com>
2025-12-18 00:52:37 +0700

    api-getAll-activity successfully

--- BE/src/main/java/com/hr_management/hr_management/controller/RunningActivityController.java
+++ BE/src/main/java/com/hr_management/hr_management/controller/RunningActivityController.java
@@ -2,6 +2,7 @@ package com.hr_management.hr_management.controller;
 
 import com.hr_management.hr_management.dto.request.ApiResponse;
 import com.hr_management.hr_management.dto.response.ActivityResponse;
+import com.hr_management.hr_management.dto.response.RunningActivityResponseDTO;
 import com.hr_management.hr_management.service.ActivityService;
 import com.hr_management.hr_management.service.RunningActivityService;
 import lombok.*;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +25,13 @@ public class RunningActivityController {
     public ApiResponse<ActivityResponse> viewDetaildActivity(@PathVariable Integer activityId){
         return ApiResponse.<ActivityResponse>builder()
                 .result(activityService.viewDetailActivity(activityId))
+                .build();
+    }
+    //Employee
+    @GetMapping()
+    public ApiResponse<List<RunningActivityResponseDTO>> getAll(){
+        return ApiResponse.<List<RunningActivityResponseDTO>>builder()
+                .result(activityService.getAllActivity())
                 .build();
     }
 
