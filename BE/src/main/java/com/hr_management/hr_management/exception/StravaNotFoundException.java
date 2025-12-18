2dfce34244f24be1fec59abe03345e9137455575
thPhonG-oss <ngthanhphong0817@gmail.com>
2025-12-18 14:22:35 +0700

    complete integration strava webhook and handle event

---
+++ BE/src/main/java/com/hr_management/hr_management/exception/StravaNotFoundException.java
@@ -0,0 +1,7 @@
+package com.hr_management.hr_management.exception;
+
+public class StravaNotFoundException extends RuntimeException {
+    public StravaNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ BE/src/main/java/com/hr_management/hr_management/service/ParticipateInService.java
@@ -0,0 +1,10 @@
+package com.hr_management.hr_management.service;
+
+import jakarta.transaction.Transactional;
+
+public interface ParticipateInService {
+    void sortCurrentParticipantsPositions(Integer runningActivityId);
+
+    @Transactional
+    void updateParticipantTotalRun(Integer participateInId, Integer additionalRun);
+}
