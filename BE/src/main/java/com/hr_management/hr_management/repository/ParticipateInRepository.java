6d3d34d3789395125189ea0c7705bee2826fefe5
Cong <vietcongtruong07@gmail.com>
2025-12-18 16:16:38 +0700

    .

--- BE/src/main/java/com/hr_management/hr_management/repository/ParticipateInRepository.java
+++ BE/src/main/java/com/hr_management/hr_management/repository/ParticipateInRepository.java
@@ -2,7 +2,7 @@ package com.hr_management.hr_management.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-package com.hr_management.hr_management.repository;
+
 
 import com.hr_management.hr_management.entity.ParticipateIn;
 import com.hr_management.hr_management.enums.ActivityStatus;
--- BE/src/main/java/com/hr_management/hr_management/service/Impl/ParticipateInServiceImpl.java
+++ BE/src/main/java/com/hr_management/hr_management/service/Impl/ParticipateInServiceImpl.java
@@ -9,7 +9,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
-package com.hr_management.hr_management.service.Impl;
+
 
 import com.hr_management.hr_management.entity.ParticipateIn;
 import com.hr_management.hr_management.repository.ParticipateInRepository;
