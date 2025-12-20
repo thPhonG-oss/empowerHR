c35164c2884fa4668568cd78af1ae0b1a606bf30
Do Ngoc Cuong <cuong01656499081@gmail.com>
2025-12-20 15:28:45 +0700

    add numberRegistered successfully
    # Conflicts:
    #	BE/src/main/java/com/hr_management/hr_management/repository/ParticipateInRepository.java

--- BE/src/main/java/com/hr_management/hr_management/mapper/RunningActivityMapper.java
+++ BE/src/main/java/com/hr_management/hr_management/mapper/RunningActivityMapper.java
@@ -3,6 +3,7 @@ package com.hr_management.hr_management.mapper;
 import com.hr_management.hr_management.dto.request.ActivityRequets;
 import com.hr_management.hr_management.dto.request.RunningActivityUpdateRequestDTO;
 import com.hr_management.hr_management.dto.response.ActivityResponse;
+import com.hr_management.hr_management.dto.response.ActivityResponseDto;
 import com.hr_management.hr_management.entity.RunningActivity;
 import org.mapstruct.Mapper;
 import org.mapstruct.*;
@@ -16,4 +17,5 @@ public interface RunningActivityMapper {
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateActivity(@MappingTarget RunningActivity runningActivity, RunningActivityUpdateRequestDTO requestDTO);
+    ActivityResponseDto toActivityResponseDto(RunningActivity activity);
 }
--- BE/src/main/java/com/hr_management/hr_management/repository/ParticipateInRepository.java
+++ BE/src/main/java/com/hr_management/hr_management/repository/ParticipateInRepository.java
@@ -9,7 +9,6 @@ import com.hr_management.hr_management.enums.ActivityStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,6 +34,8 @@ public interface ParticipateInRepository extends JpaRepository<ParticipateIn, In
     Long countByRunningActivityAndIsCancelledFalse(RunningActivity activity);
 
     Optional<ParticipateIn> findByParticipateInIdAndEmployee_EmployeeId(Integer participateId,Integer employeeId);
+
+    Integer countByRunningActivity_RunningActivityIdAndIsCancelledFalse(Integer runningActivityId);
     List<ParticipateIn> findByEmployee_EmployeeId(Integer employeeId);
 
     Optional<ParticipateIn> findByEmployee_EmployeeIdAndRunningActivity_RunningActivityId(Integer employeeEmployeeId, Integer runningActivityRunningActivityId);
