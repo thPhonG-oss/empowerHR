543b000e0674a57580dff7936645e94367b1a09c
Cong <vietcongtruong07@gmail.com>
2025-12-23 03:24:26 +0700

    Add Axios runningActivitiesApi

--- FE/src/api/runningActivityApi.js
+++ FE/src/api/runningActivityApi.js
@@ -5,6 +5,22 @@ const runningActivityApi = {
     axiosClient.get(`/api/v1/activities/admin`, {
       params: { pageNumber, pageSize },
     }),
+  
+  employeeGetAllOpeningActivity: () => axiosClient.get(`/api/v1/activities`),
+
+  employeeGetAllRegisteredActivity: (employeeID) => axiosClient.get(`/api/v1/employees/${employeeID}/activities`,{
+    params:{employeeID}
+  }),
+
+  employeeRegisterActivity: (activityId) =>
+    axiosClient.post(`/api/v1/participateIn/register/${activityId}`,{
+      params:{activityId}
+    }),
+
+  employeeUnregisterActivity: (activityId) =>
+    axiosClient.delete(`/api/v1/participateIn/${activityId}`,{
+      params:{activityId}
+    }),
 };
 
 export default runningActivityApi;
