0aa9b5ec721ac36c13eb1ad6ceba41781154043c
daominhchien <daominhchien2k4@gmail.com>
2026-01-12 13:59:21 +0700

    Update Api getAllPointAccounts ( thêm employeeId vào response )

--- BE/src/main/java/com/hr_management/hr_management/mapper/PointAccountMapper.java
+++ BE/src/main/java/com/hr_management/hr_management/mapper/PointAccountMapper.java
@@ -4,11 +4,13 @@ import com.hr_management.hr_management.dto.response.PointAccountResponse;
 import com.hr_management.hr_management.dto.response.PointAccountResponseDTO;
 import com.hr_management.hr_management.entity.PointAccount;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PointAccountMapper {
 
     PointAccountResponseDTO toPointAccountResponseDTO(PointAccount pointAccount);
 
+    @Mapping(target = "employeeId", source ="employee.employeeId" )
     PointAccountResponse toPointAccountResponse(PointAccount pointAccount);
 }
